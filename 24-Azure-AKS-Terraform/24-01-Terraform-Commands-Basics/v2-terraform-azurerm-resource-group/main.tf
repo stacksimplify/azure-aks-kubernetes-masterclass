a4bd059bd0d3079aa742f67c082efefdf9b5b7d8
Kalyan Reddy Daida <stacksimplify@gmail.com>
2020-11-25 17:03:28 +0530

    Welcome to Stack Simplify

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -10,13 +10,14 @@ provider "azurerm" {
  
 # Create a Azure Resource Group
 resource "azurerm_resource_group" "aks-rg2" {
-  name     = "aks-rg2-tf2"
+  name     = "aks-rg2-tf"
   location = "Central US"
 
 # Add Tags
-  tags = {
-    "environment" = "k8sdev"
-  }
+#  tags = {
+#    "environment" = "k8sdev"
+#    "demotag"     = "refreshtest"
+#  }
 
 }
 
