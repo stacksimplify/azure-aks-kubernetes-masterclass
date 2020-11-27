d73fbe0fcd587cae6775c23b9b9806fa66bc68ab
Kalyan Reddy Daida <stacksimplify@gmail.com>
2020-11-27 15:50:51 +0530

    Welcome to Stack Simplify

---
+++ 24-Azure-AKS-Terraform/24-03-Create-AKS-Cluster/terraform-manifests-aks/backup/08-outputs.tf
@@ -0,0 +1,32 @@
+# Create Outputs
+# 1. Resource Group Location
+# 2. Resource Group Id
+# 3. Resource Group Name
+
+# Resource Group Outputs
+output "location" {
+  value = azurerm_resource_group.aks_rg.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.aks_rg.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
+}
+
+# Azure AKS Outputs
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "aks_cluster_kubernetes_version" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+}
+
